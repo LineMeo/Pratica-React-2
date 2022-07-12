c4572417cd5b50db942c996021913677216b27bd
Aline Marques <79379302+LineMeo@users.noreply.github.com>
2022-07-12 16:15:09 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Faça login no CodeSandbox
+
+// Crie um novo sandbox em REACT
+
+// Crie um Component de class que retorne uma frase na tela
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Trabalhando com React no módulo 2 - VnW</h1>
+      </div>
+    );
+  }
+}
